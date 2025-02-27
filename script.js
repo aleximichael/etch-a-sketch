81df6ca1df7432fbc814906664c793467f865b5c
Alexi Michael <189779678+heyitsalexi@users.noreply.github.com>
2025-02-27 15:09:42 -0500

    Add: Feature to modify the grid size

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gridContainer = document.querySelector("#grid-container");
+const gridContainer = document.querySelector(".grid-container");
 
 const col = document.createElement("div");
 const grid = document.createElement("div");
@@ -6,21 +6,32 @@ const grid = document.createElement("div");
 col.classList.add("col-container");
 grid.classList.add("grid");
 
-for (let i = 1; i < 17; i++) {
-    let currCol = col.cloneNode(true);
-    gridContainer.appendChild(currCol);
+function createGrid(defaultSize = 16) {
+    for (let i = 1; i < defaultSize+1; i++) {
+        let currCol = col.cloneNode(true);
+        currCol.classList.add("col-" + i);
+        gridContainer.appendChild(currCol);
+    
+        for (let j = 1; j < defaultSize+1; j++) {
+            currGrid = grid.cloneNode(true);
+            currCol.appendChild(currGrid);
+        }
+    };
 
-    for (let j = 1; j < 17; j++) {
-        currGrid = grid.cloneNode(true);
-        currCol.appendChild(currGrid);
-    }
-};
+    const grids = document.querySelectorAll(".grid");
 
-const grids = document.querySelectorAll(".grid");
-
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", () => {
-        grid.style.backgroundColor = "blue";
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseover", () => {
+            grid.style.backgroundColor = "blue";
+        });
     });
+}
+
+const gridBtn = document.querySelector(".grid-size");
+
+gridBtn.addEventListener("click", ()=> {
+    gridContainer.replaceChildren();
+    createGrid(+prompt("How many squares wide?"));
 });
 
+createGrid();

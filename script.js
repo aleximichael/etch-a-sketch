922d0671308e151879f3c1a0b97e055f05467c80
Alexi Michael <189779678+heyitsalexi@users.noreply.github.com>
2025-02-28 23:41:35 -0500

    Add: Buttons to reset grid and change colours

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
 const gridContainer = document.querySelector(".grid-container");
-
 const column = document.createElement("div");
-const grid = document.createElement("div");
+const square = document.createElement("div");
+square.classList.add("square");
 
-column.classList.add("col-container");
-grid.classList.add("grid");
-
-function createGrid(defaultSize = 16) {
-    for (let i = 1; i < defaultSize+1; i++) {
+function createGrid(numSquares = 16) {
+    for (let i = 1; i < numSquares+1; i++) {
         let currCol = column.cloneNode(true);
         gridContainer.appendChild(currCol);
     
-        for (let j = 1; j < defaultSize+1; j++) {
-            currCol.appendChild(grid.cloneNode(true));
+        for (let j = 1; j < numSquares+1; j++) {
+            currCol.appendChild(square.cloneNode(true));
         };
     };
 
-    const grids = document.querySelectorAll(".grid");
+    let squareWidth = `${600 / numSquares}px`;
 
-    let alpha = 0;
+    const colourBtn = document.querySelector(".change-colour");
+    let colourToggle = false;
 
-    grids.forEach((grid) => {
-        grid.addEventListener("mouseover", () => {
-            grid.style.backgroundColor = getRandomColour(alpha);
-            alpha < 1 ? alpha += 0.1 : alpha = 0;
+    colourBtn.addEventListener("click", () => {
+        colourToggle = true;
+    })
+
+    let grids = document.querySelectorAll(".square");
+
+    grids.forEach((square) => {
+        square.setAttribute("style", `width: ${squareWidth}; height: ${squareWidth}`);
+        square.addEventListener("mouseover", () => {
+            if (!colourToggle) {
+                square.style.backgroundColor = "black";
+            } else {
+                square.style.backgroundColor = getRandomColour(1);
+            }
         });
     });
-}
+    
+};
+
+const sizeBtn = document.querySelector(".change-size");
+
+sizeBtn.addEventListener("click", () => {
+    newGridSize = +prompt("How many squares wide?");
+    console.log(newGridSize);
+    if (newGridSize > 100) {
+        alert("ERROR. Maximum of 100 squares.");
+    } else if (isNaN(newGridSize)) {
+        alert("ERROR. Please enter a number.");
+    // If the user cancels the pop-up - nothing happens
+    } else if (newGridSize === 0) { 
+    } else {
+        gridContainer.replaceChildren();
+        createGrid(newGridSize);
+    };
+});
+
+const resetBtn = document.querySelector(".reset-grid");
+
+resetBtn.addEventListener("click", () => {
+    let grids = document.querySelectorAll(".square");
+    grids.forEach((square) => {
+        square.style.backgroundColor = "white";
+    });
+});
 
 function getRandomColour(alpha) {
     let r = Math.floor(Math.random()*255);
@@ -34,20 +69,5 @@ function getRandomColour(alpha) {
     let b = Math.floor(Math.random()*255);
     return `rgb(${r}, ${g}, ${b}, ${alpha})`;
 }
-
-const gridBtn = document.querySelector(".reset-button");
-
-gridBtn.addEventListener("click", ()=> {
-    newGridSize = +prompt("How many squares wide?");
-    console.log(newGridSize);
-    if (newGridSize > 100) {
-        alert("ERROR. Maximum of 100 squares.");
-    } else if (isNaN(newGridSize)) {
-        alert("ERROR. Please enter a number.");
-    } else {
-        gridContainer.replaceChildren();
-        createGrid(newGridSize);
-    };
-});
 
 createGrid();
